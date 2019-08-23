5abb9896ce2d4dec85cef361281b255b71b2de3c
Max Wilson <maxjw98@gmail.com>
2019-08-23 10:42:35 +0100

    Refactoring

--- hello.go
+++ hello.go
@@ -6,6 +6,9 @@ const englishHelloPrefix = "Hello, "
 
 //Hello - Print out something
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name
 }
 
