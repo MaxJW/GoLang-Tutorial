efb3638a3a50cf0181ab830f300b08b6f46e3bb6
Max Wilson <maxjw98@gmail.com>
2019-08-27 10:37:26 +0100

    Hello world to localhost using Dependency Injection

--- dep-inj/di.go
+++ dep-inj/di.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"net/http"
 )
 
-func Greet(write *bytes.Buffer, name string) {
-	fmt.Printf("Hello, %s", name)
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
