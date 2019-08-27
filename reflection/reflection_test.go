50c704ea2faebda3695726d3f71853f2ba52fe08
Max Wilson <maxjw98@gmail.com>
2019-08-27 16:12:19 +0100

    Finished all tests

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -77,6 +77,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Dundee", "London"},
 		},
+		{
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
 	}
 
 	for _, test := range cases {
@@ -92,4 +100,30 @@ func TestWalk(t *testing.T) {
 		})
 	}
 
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contains %q but it didnt", haystack, needle)
+	}
 }
