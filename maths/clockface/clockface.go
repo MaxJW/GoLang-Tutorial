05f418b08aa811159a885cbe0ae2cbb485c6afdf
Max Wilson <maxjw98@gmail.com>
2019-08-29 10:24:29 +0100

    Minute hand added

--- maths/clockface/clockface.go
+++ maths/clockface/clockface.go
@@ -11,26 +11,24 @@ type Point struct {
 	Y float64
 }
 
-const secondHandLength = 90
-const clockCentreX = 150
-const clockCentreY = 150
-
-// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
-// represented as a Point.
-func SecondHand(t time.Time) Point {
-	p := secondHandPoint(t)
-	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
-	p = Point{p.X, -p.Y}
-	p = Point{p.X + clockCentreX, p.Y + clockCentreY} //translate
-	return p
-}
-
 func secondsInRadians(t time.Time) float64 {
-	return (math.Pi / (30 / (float64(t.Second()))))
+	return (math.Pi / (30 / float64(t.Second())))
 }
 
 func secondHandPoint(t time.Time) Point {
-	angle := secondsInRadians(t)
+	return angleToPoint(secondsInRadians(t))
+}
+
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / 60) +
+		(math.Pi / (30 / float64(t.Minute())))
+}
+
+func minuteHandPoint(t time.Time) Point {
+	return angleToPoint(minutesInRadians(t))
+}
+
+func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
 
